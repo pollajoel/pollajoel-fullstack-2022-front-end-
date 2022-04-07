f6ce898ccfdac8ea769162d0c40ea5e8b0c7abca
pollajoel <pollajoel2017@gmail.com>
2022-04-07 11:12:59 +0200

    update upload file to the

--- components/userForm/userForm.js
+++ components/userForm/userForm.js
@@ -122,7 +122,7 @@ export default function UserForm(props) {
             var formdata = new FormData()
             formdata.append("file", e.target.files[0], e.target.value);
             var requestOptions = { method: 'POST',body: formdata,};
-            fetch("http://localhost:3005/api/upload", requestOptions)
+            fetch("https://pollajoel-fullstack-2022-front-end.vercel.app/api/upload", requestOptions)
             .then(response => response.text())
             .then(result =>{
                 if( result){ 
